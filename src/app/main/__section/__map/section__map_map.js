62b9da14db7bb873e423642c20bd4459a87b353e
Anton Yatsenko <mip737@yandex.ru>
2020-04-28 16:47:45 +0300

    Кластеризация добавлена + исправление стилей балуна

--- src/app/main/__section/__map/section__map_map.js
+++ src/app/main/__section/__map/section__map_map.js
@@ -8,11 +8,35 @@ function init() {
     var myMap = new ymaps.Map("map", {
         center: [48.678298, 13.919976],
         zoom: 5,
-        controls: ['zoomControl']
+        controls: ['zoomControl'],
     }, {
         suppressMapOpenBlock: true
     });
 
+    var clusterer = new ymaps.Clusterer({
+        /**
+         * Через кластеризатор можно указать только стили кластеров,
+         * стили для меток нужно назначать каждой метке отдельно.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/option.presetStorage.xml
+         */
+        preset: 'islands#invertedVioletClusterIcons',
+        /**
+         * Ставим true, если хотим кластеризовать только точки с одинаковыми координатами.
+         */
+        groupByCoordinates: false,
+        /**
+         * Опции кластеров указываем в кластеризаторе с префиксом "cluster".
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ClusterPlacemark.xml
+         */
+        clusterDisableClickZoom: true,
+        clusterHideIconOnBalloonOpen: false,
+        geoObjectHideIconOnBalloonOpen: false,
+        clusterIconLayout: ymaps.templateLayoutFactory.createClass('<div class="clusterIcon">{{properties.geoObjects[0].properties._data.name}}</div>'),
+        clusterIconShape: {
+            type: 'Rectangle',
+            coordinates: [[0, 0], [20, 20]]
+        }
+    });
 
     var IconContentLayout = ymaps.templateLayoutFactory.createClass(
         '<div class="mark">$[properties.iconContent]</div>'
@@ -20,154 +44,58 @@ function init() {
 
     var IconContentLayoutHover = ymaps.templateLayoutFactory.createClass(
         '<div class="mark_hover">' +
-            '$[properties.balloonHeader]' +
+        '$[properties.balloonHeader]' +
         '</div>'
     );
 
     var MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
-            ' <div class="popover top">' +
-            ' <a class="close" href="#"><div class="close"></div></a>' +
-            ' <div class="arrow"> </div>' +
-            ' <div class="popover-inner">' +
-            '$[[options.contentLayout observeSize minWidth=235 maxWidth=1200 maxHeight=350]]' +
-            ' </div>' +
-            ' </div>', {
-                /**
-                 * Строит экземпляр макета на основе шаблона и добавляет его в родительский HTML-элемент.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#build
-                 * @function
-                 * @name build
-                 */
-                build: function () {
-                    this.constructor.superclass.build.call(this);
-                    this._$element = $('.popover', this.getParentElement());
-                    this.applyElementOffset();
-                    this._$element.find('.close')
-                        .on('click', $.proxy(this.onCloseClick, this));
-                },
-                /**
-                 * Удаляет содержимое макета из DOM.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#clear
-                 * @function
-                 * @name clear
-                 */
-                clear: function () {
-                    this._$element.find('.close')
-                        .off('click');
-                    this.constructor.superclass.clear.call(this);
-                },
+        ' <div class="popover top">' +
+        ' <a class="close" href="#"><div class="close"></div></a>' +
+        ' <div class="arrow"> </div>' +
+        ' <div class="popover-inner">' +
+        '$[[options.contentLayout observeSize minWidth=235 maxWidth=1200 maxHeight=350]]' +
+        ' </div>' +
+        ' </div>');
 
-                /**
-                 * Метод будет вызван системой шаблонов АПИ при изменении размеров вложенного макета.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                 * @function
-                 * @name onSublayoutSizeChange
-                 */
-                onSublayoutSizeChange: function () {
-                    MyBalloonLayout.superclass.onSublayoutSizeChange.apply(this, arguments);
+    // Создание вложенного макета содержимого балуна.
+    var MyBalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div class="container text-white">' +
+        '<div class="column">$[properties.balloonHeader]</div>' +
+        '<div class="column">$[properties.balloonContent] </div>' +
+        '</div>'
+    );
 
-                    if (!this._isElement(this._$element)) {
-                        return;
-                    }
-                    this.applyElementOffset();
-                    this.events.fire('shapechange');
-                },
-
-                /**
-                 * Сдвигаем балун, чтобы середина указывала на точку привязки.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                 * @function
-                 * @name applyElementOffset
-                 */
-                applyElementOffset: function () {
-                    this._$element.css({
-                        left: -(this._$element[0].offsetWidth / 2),
-                        top: -(this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight)
-                    });
-                },
-
-                /**
-                 * Закрывает балун при клике на крестик, кидая событие "userclose" на макете.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                 * @function
-                 * @name onCloseClick
-                 */
-                onCloseClick: function (e) {
-                    e.preventDefault();
-                    this.events.fire('userclose');
-                },
-
-                /**
-                 * Используется для автопозиционирования (balloonAutoPan).
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ILayout.xml#getClientBounds
-                 * @function
-                 * @name getClientBounds
-                 * @returns {Number[][]} Координаты левого верхнего и правого нижнего углов шаблона относительно точки привязки.
-                 */
-                getShape: function () {
-                    if (!this._isElement(this._$element)) {
-                        return MyBalloonLayout.superclass.getShape.call(this);
-                    }
-                    var position = this._$element.position();
-                    return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
-                        [position.left, position.top],
-                        [
-                            position.left + this._$element[0].offsetWidth,
-                            position.top + this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight
-                        ]
-                    ]));
-                },
-                /**
-                 * Проверяем наличие элемента (в ИЕ и Опере его еще может не быть).
-                 * @function
-                 * @private
-                 * @name _isElement
-                 * @param {jQuery} [element] Элемент.
-                 * @returns {Boolean} Флаг наличия.
-                 */
-                _isElement: function (element) {
-                    return element && element[0] && element.find('.arrow')[0];
-                }
-            }),
-
-        // Создание вложенного макета содержимого балуна.
-        MyBalloonContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div class="container text-white">' +
-            '<div class="column">$[properties.balloonHeader]</div>' +
-            '<div class="column">$[properties.balloonContent] </div>' +
-            '</div>'
-        );
-
+    let placemarks = [];
 
     const renderMarks = async () => {
         const data = await new getStatisticData();
         const resData = await data.byTotalCases();
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 50; i++) {
             let coord = await func(resData[i].country);
             if (coord) {
                 var myPlacemarkWithContent = new ymaps.Placemark([coord[1], coord[0]], {
                     iconContent: `${resData[i].cases.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`,
-                    balloonHeader: 
-                    '<div class="placemark_info">' +
-                        '<div class="placemark_info-cases">' + 
-                            `<h2>${resData[i].country}</h2>` +
+                    balloonHeader: '<div class="placemark_info">' +
+                        '<div class="placemark_info-cases">' +
+                        `<h2>${resData[i].country}</h2>` +
                         '</div>' +
-                        '<div class="placemark_info-cases">' + 
-                            `<h3>New cases: <span>${resData[i].cases.new.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span></h3>` +
+                        '<div class="placemark_info-cases">' +
+                        `<h3>New cases: <span>${resData[i].cases.new ? resData[i].cases.new.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : 0}</span></h3>` +
                         '</div>' +
-                        '<div class="placemark_info-cases">' + 
-                            `<h3>Critical cases: <span>${resData[i].cases.critical.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span></h3>` +
+                        '<div class="placemark_info-cases">' +
+                        `<h3>Critical cases: <span>${resData[i].cases.critical ? resData[i].cases.critical.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : 0}</span></h3>` +
                         '</div>' +
-                        '<div class="placemark_info-cases">' + 
-                            `<h3>Total cases: <span>${resData[i].cases.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span></h3>` +
+                        '<div class="placemark_info-cases">' +
+                        `<h3>Total cases: <span>${resData[i].cases.total ? resData[i].cases.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : 0}</span></h3>` +
                         '</div>' +
-                        '<div class="placemark_info-cases">' + 
-                            `<h3>Recovered: <span>${resData[i].cases.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span></h3>` +
+                        '<div class="placemark_info-cases">' +
+                        `<h3>Recovered: <span>${resData[i].cases.recovered ? resData[i].cases.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : 0}</span></h3>` +
                         '</div>' +
-                        '<div class="placemark_info-cases">' + 
-                            `<h3>Deaths: <span>${resData[i].deaths.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span></h3>` +
+                        '<div class="placemark_info-cases">' +
+                        `<h3>Deaths: <span>${resData[i].deaths.total ? resData[i].deaths.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : 0}</span></h3>` +
                         '</div>' +
-                    '</div>',
+                        '</div>',
+                    name: `${resData[i].cases.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`
                 }, {
                     iconLayout: 'default#imageWithContent',
                     iconImageHref: '',
@@ -180,28 +108,34 @@ function init() {
                 });
                 let time;
                 myPlacemarkWithContent.events
-                    .add('click', function(e) {
+                    .add('click', function (e) {
                         e.get('target').options.set('iconContentLayout', IconContentLayoutHover);
-                        e.get('target').options.set('iconImageSize', [170, 170])
+                        e.get('target').options.set('iconImageSize', [220, 250])
                     })
                     .add('mouseenter', function (e) {
                         time = setInterval(() => {
                             e.get('target').options.set('iconContentLayout', IconContentLayoutHover);
                         }, 500)
-                        e.get('target').options.set('iconImageSize', [170, 170])
+                        e.get('target').options.set('iconImageSize', [220, 250])
                     })
                     .add('mouseleave', function (e) {
                         clearInterval(time);
                         e.get('target').options.set('iconContentLayout', IconContentLayout);
                         e.get('target').options.set('iconImageSize', [65, 65])
                     });
+                placemarks.push(myPlacemarkWithContent);
                 myMap.geoObjects.add(myPlacemarkWithContent);
             }
         }
+        clusterer.add(placemarks);
     }
 
-
     renderMarks();
+    clusterer.options.set({
+        gridSize: 128,
+        clusterDisableClickZoom: true,
+    });
+    myMap.geoObjects.add(clusterer);
 
     const func = async (country) => {
         const req = await fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=a712e72d-d189-4c89-a4e8-4e14a748b2c4&format=json&geocode=${country}`);
